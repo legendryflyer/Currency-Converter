11ddd1b7958f2b39d33b6f6c84bb9b8d91e7f530
legendryflyer <tavish.anade@gmail.com>
2024-07-28 22:32:15 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ConverterForm from "./components/ConverterForm";
+
+const App = () => {
+  return (
+    <div className="currency-converter">
+      <h2 className="converter-title">Currency Converter</h2>
+      <ConverterForm />
+    </div>
+  );
+};
+
+export default App;
