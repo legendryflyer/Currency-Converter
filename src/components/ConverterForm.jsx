5d63470c345fe764facc489baa538c31a340bc31
legendryflyer <tavish.anade@gmail.com>
2024-07-28 23:50:40 +0530

    added

--- src/components/ConverterForm.jsx
+++ src/components/ConverterForm.jsx
@@ -1,32 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CurrencySelect from "./CurrencySelect";
 
 const ConverterForm = () => {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [result, setResult] = useState("");
+  const [isLoadind, setIsLoading] = useState(false);
+
+  const handleSwapCurrrency = () => {
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
+
+  const getExchangeRate = async () => {
+    const apiKEY = import.meta.env.VITE_APP_API_KEY;
+    const apiURL = `https://v6.exchangerate-api.com/v6/${apiKEY}/pair/${fromCurrency}/${toCurrency}`;
+
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(apiURL);
+      if (!response.ok) {
+        throw Error("Failed to fetch exchange rate");
+      }
+      const data = await response.json();
+      const rate = data.conversion_rate * amount;
+      //   console.log(rate);
+      setResult(`${amount} ${fromCurrency}=${rate.toFixed(3)} ${toCurrency}`);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    getExchangeRate();
+  };
+
+  useEffect(() => getExchangeRate, []);
   return (
-    <form className="converter-form">
+    <form className="converter-form" onSubmit={handleFormSubmit}>
       <div className="form-group">
         <label className="form-label">Enter Amount</label>
-        <input type="number" className="form-input" required />
+        <input
+          type="number"
+          className="form-input"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          required
+        />
       </div>
       <div className="form-group form-currency-group">
         <div className="form-section">
           <label className="form-label">Form</label>
-          <div className="currency-select">
-            <img
-              src="https://flagsapi.com/US/flat/64.png"
-              alt="Flag"
-              srcset=""
-            />
-            <select className="currency-dropdown">
-              <option value="USD" selected>
-                USD
-              </option>
-              <option value="EUR">EUR</option>
-              <option value="GBP">GBP</option>
-            </select>
-          </div>
+          <CurrencySelect
+            selectedCurrency={fromCurrency}
+            handleCurrency={(e) => setFromCurrency(e.target.value)}
+          />
         </div>
 
-        <div className="swap-icon">
+        <div className="swap-icon" onClick={handleSwapCurrrency}>
           <svg
             width="16"
             viewBox="0 0 20 19"
@@ -41,26 +77,21 @@ const ConverterForm = () => {
 
         <div className="form-section">
           <label className="form-label">To</label>
-          <div className="currency-select">
-            <img
-              src="https://flagsapi.com/BE/flat/64.png"
-              alt="Flag"
-              srcset=""
-            />
-            <select className="currency-dropdown">
-              <option value="USD">USD</option>
-              <option value="EUR" selected>
-                EUR
-              </option>
-              <option value="GBP">GBP</option>
-            </select>
-          </div>
+          <CurrencySelect
+            selectedCurrency={toCurrency}
+            handleCurrency={(e) => setToCurrency(e.target.value)}
+          />
         </div>
       </div>
-      <button type="submit" className="submit-button">
+      <button
+        type="submit"
+        className={`${isLoadind ? "loading" : ""} submit-button`}
+      >
         Get Exchange Rate
       </button>
-      <p className="exchange-rate-result">1 USD = 0.9 EUR</p>
+      <p className="exchange-rate-result">
+        {isLoadind ? "Getting Exchange Rate...." : result}
+      </p>
     </form>
   );
 };
